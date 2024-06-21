8ae4bbead40089350718ab073b6e018c54823fa1
Trần Quang Huy <huy.com2003@gmail.com>
2024-06-21 21:42:40 +0700

    Update

--- DesignAPP/assets/data/Singleton.js
+++ DesignAPP/assets/data/Singleton.js
@@ -1,41 +1,74 @@
 /* eslint-disable prettier/prettier */
-import SingletonImage from '../images/Singleton/singleton.png'
-import Characteristics from '../images/Singleton/Characteristics.png'
-import Motivation1 from '../images/Singleton/Motivation_1.png'
-import Motivation2 from '../images/Singleton/Motivation_2.png'
-import Motivation3 from '../images/Singleton/Motivation_3.png'
-export const Singleton_En = {
-    Overview_1 : 'Singleton.',
-    Overview_2 : 'Thuộc phân nhóm Creational patterns.',
-    Overview_3 : 'Kiểm soát việc tạo lập đối tượng.',
-    Overview_4 : 'Luôn luôn chỉ có 1 instance duy nhất cho Class. Instance này phải được truy xuất ở phạm vi Global.',
-    Overview_5 : 'Luôn luôn sẵn sàng để được sử dụng.',
-    Motivation_1: 'Cần duy nhất một instance của một class nào đó trong system.',
-    Motivation_2: 'Cần truy cập, sử dụng instance này dễ dàng.',
-    Motivation_3: 'window manager, Audio device, Printer spooler, những tài nguyên dùng chung (DB, file,...) chỉ cần dùng 1 instance mỗi lần được request.',
-    Characteristics_1: 'Dữ liệu thành viên instance (private và static) là đối tượng duy nhất của lớp Singleton.',
-    Characteristics_2: 'Constructor của lớp Singleton được định nghĩa thành protected hoặc private.',
-    Characteristics_3: 'Phương thức getInstance() dùng để khởi tạo đối tượng duy nhất, định nghĩa thành public và static. Client chỉ dùng getInstance() để tạo đối tượng cho lớp Singleton.',
-    Consequences_1: 'Đảm bảo truy cập vào duy nhất một instance.',
-    Consequences_2: 'Đối tượng singleton chỉ được khởi tạo duy nhất một lần khi nó được yêu cầu lần đầu.',
-    Consequences_3: 'Linh hoạt hơn một class bình thường.',
-    Consequences_4: 'Pattern này yêu cầu được đối xử đặc biệt trong môi trường multithreaded để nhiều threads không thể tạo một đối tượng singleton nhiều lần.',
-    Consequences_5: 'Gây khó khăn trong kiểm thử',
-    Reality_1: 'Eager initialization',
-    Reality_2: 'Singleton Class được khởi tạo ngay khi được gọi đến.',
-    Reality_3: 'Static block initialization',
-    Reality_4: 'Static block cung cấp thêm lựa chọn cho việc handle exception hay các xử lý khác.',
-    Reality_5: 'Lazy Initialization',
-    Reality_6: 'Là một cách làm mang tính mở rộng hơn so với 2 cách làm trên và hoạt động tốt trong môi trường đơn luồng (single-thread).',
-    Reality_7: 'Thread Safe Singleton',
-    Reality_8: 'Phương thức synchronized của hàm getInstance()',
-    Reality_9: 'Double Check Locking Singleton',
-    Reality_10: 'Khai báo volatile cho instance để tránh lớp làm việc không chính xác do quá trình tối ưu hóa của trình biên dịch',
-    Reality_11: 'Bill Pugh Singleton',
-    Reality_12: 'Tạo ra static nested class với vai trò 1 Helper khi muốn tách biệt chức năng cho 1 class function rõ ràng hơn.',
-    Image_1: SingletonImage,
-    Image_2: Motivation1,
-    Image_3: Motivation2,
-    Image_4: Motivation3,
-    Image_5: Characteristics,
+import SingletonImage from '../images/Singleton/singleton.png';
+import Characteristics from '../images/Singleton/Characteristics.png';
+import Motivation1 from '../images/Singleton/Motivation_1.png';
+import Motivation2 from '../images/Singleton/Motivation_2.png';
+import Motivation3 from '../images/Singleton/Motivation_3.png';
+export const Singleton_Vn = {
+    Intent: [
+        'Singleton is a creational design pattern that lets you ensure that a class has only one instance, while providing a global access point to this instance.',
+    ],
+    Problem: [
+        'The Singleton pattern solves two problems at the same time, violating the Single Responsibility Principle:',
+        '1.Ensure that a class has just a single instance. Why would anyone want to control how many instances a class has? The most common reason for this is to control access to some shared resource—for example, a database or a file.',
+        'Here`s how it works: imagine that you created an object, but after a while decided to create a new one. Instead of receiving a fresh object, you will get the one you already created.',
+        'Note that this behavior is impossible to implement with a regular constructor since a constructor call must always return a new object by design.',
+        '2. Provide a global access point to that instance. Remember those global variables that you (all right, me) used to store some essential objects? While they’re very handy, they’re also very unsafe since any code can potentially overwrite the contents of those variables and crash the app.',
+        'Just like a global variable, the Singleton pattern lets you access some object from anywhere in the program. However, it also protects that instance from being overwritten by other code.',
+        'There’s another side to this problem: you don’t want the code that solves problem #1 to be scattered all over your program. It’s much better to have it within one class, especially if the rest of your code already depends on it.',
+        'Nowadays, the Singleton pattern has become so popular that people may call something a singleton even if it solves just one of the listed problems.',
+    ],
+    Solution: [
+        'All implementations of the Singleton have these two steps in common:',
+        'Make the default constructor private, to prevent other objects from using the new operator with the Singleton class.',
+        'Create a static creation method that acts as a constructor. Under the hood, this method calls the private constructor to create an object and saves it in a static field. All following calls to this method return the cached object.',
+        'If your code has access to the Singleton class, then it’s able to call the Singleton’s static method. So whenever that method is called, the same object is always returned.',
+    ],
+    RealWorldAnalogy: [
+        'The government is an excellent example of the Singleton pattern. A country can have only one official government. Regardless of the personal identities of the individuals who form governments, the title, “The Government of X”, is a global point of access that identifies the group of people in charge.'
+    ],
+    Structure:[
+        '1. The Singleton class declares the static method getInstance that returns the same instance of its own class.',
+        'The Singleton class declares the static method getInstance that returns the same instance of its own class.',
+    ],
+    Applicability: [
+        'Use the Singleton pattern when a class in your program should have just a single instance available to all clients; for example, a single database object shared by different parts of the program.',
+        'The Singleton pattern disables all other means of creating objects of a class except for the special creation method. This method either creates a new object or returns an existing one if it has already been created.',
+        ' Use the Singleton pattern when you need stricter control over global variables.',
+        'Unlike global variables, the Singleton pattern guarantees that there’s just one instance of a class. Nothing, except for the Singleton class itself, can replace the cached instance.',
+        'Note that you can always adjust this limitation and allow creating any number of Singleton instances. The only piece of code that needs changing is the body of the getInstance method.',
+    ],
+    HowtoImplement:[
+        '1. Add a private static field to the class for storing the singleton instance.',
+        '2. Declare a public static creation method for getting the singleton instance. ',
+        '3. Implement “lazy initialization” inside the static method. It should create a new object on its first call and put it into the static field. The method should always return that instance on all subsequent calls.',
+        '4. Make the constructor of the class private. The static method of the class will still be able to call the constructor, but not the other objects.',
+        '5. Go over the client code and replace all direct calls to the singleton’s constructor with calls to its static creation method.',
+    ],
+    Pros:[
+        'You can be sure that a class has only a single instance.',
+        'You gain a global access point to that instance.',
+        'The singleton object is initialized only when it’s requested for the first time.',
+    ],
+    Cons:[
+        'Violates the Single Responsibility Principle. The pattern solves two problems at the time.',
+        'The Singleton pattern can mask bad design, for instance, when the components of the program know too much about each other.',
+        'The pattern requires special treatment in a multithreaded environment so that multiple threads won’t create a singleton object several times.',
+        'It may be difficult to unit test the client code of the Singleton because many test frameworks rely on inheritance when producing mock objects. Since the constructor of the singleton class is private and overriding static methods is impossible in most languages, you will need to think of a creative way to mock the singleton. Or just don’t write the tests. Or don’t use the Singleton pattern.',
+    ],
+    RelationswithOtherPatterns:[
+        'A Facade class can often be transformed into a Singleton since a single facade object is sufficient in most cases.',
+        'Flyweight would resemble Singleton if you somehow managed to reduce all shared states of the objects to just one flyweight object. But there are two fundamental differences between these patterns:',
+        '1. There should be only one Singleton instance, whereas a Flyweight class can have multiple instances with different intrinsic states.',
+        '2. The Singleton object can be mutable. Flyweight objects are immutable.',
+        'Abstract Factories, Builders and Prototypes can all be implemented as Singletons.',
+    ],
+    Image: [
+        SingletonImage,
+        Motivation1,
+        Motivation2,
+        Motivation3,
+        Characteristics,
+    ]
 }
+
--- DesignAPP/screens/SingletonScreen.js
+++ DesignAPP/screens/SingletonScreen.js
@@ -1,21 +1,56 @@
 /* eslint-disable prettier/prettier */
-import { View, Text, ScrollView, Image, TouchableOpacity, StyleSheet, FlatList,TouchableWithoutFeedback, Animated } from 'react-native'
-import React, { useEffect, useState } from 'react'
+import { View, Text, ScrollView, Image, TouchableOpacity, StyleSheet, FlatList,TouchableWithoutFeedback, Animated } from 'react-native';
+import React, { useEffect, useState } from 'react';
 
-import { Singleton_En } from '../assets/data/Singleton.js'
-import { PinchGestureHandler, State } from 'react-native-gesture-handler'
+import { Singleton_Vn } from '../assets/data/Singleton.js';
+import { PinchGestureHandler, State } from 'react-native-gesture-handler';
 const SingletonScreen = ({navigation, route}) => {
     const content_text = (itext) =>{
         return(
             <Text style={styles.content}>
-                - {itext}
+                {itext}
             </Text>
         );
     };
-    const highlight_content_text = (itext) =>{
+    const dot_content_text = (itext) =>{
         return(
-            <Text style={styles.content_highlight}>
+            <Text style={styles.contentDot}>
+                • {itext}
+            </Text>
+        );
+    };
+    const num_content_text = (itext) =>{
+        return(
+            <Text style={styles.contentNum}>
                 {itext}
+            </Text>
+        );
+    };
+    const greenTick_content_text = (itext) =>{
+        return(
+            <Text style={styles.contentNum}>
+                <Image style={styles.pros_cons_icon} source={require('../assets/icon/greenTick.png')}/>{itext}
+            </Text>
+        );
+    };
+    const redX_content_text = (itext) =>{
+        return(
+            <Text style={styles.contentNum}>
+                <Image style={styles.pros_cons_icon} source={require('../assets/icon/redX.png')}/>{itext}
+            </Text>
+        );
+    };
+    const bug_content_text = (itext) =>{
+        return(
+            <Text style={styles.contentNum}>
+                <Image style={styles.ApplicabilityContent} source={require('../assets/icon/Applicability1.png')}/>{itext}
+            </Text>
+        );
+    };
+    const lightN_content_text = (itext) =>{
+        return(
+            <Text style={styles.contentNum}>
+                <Image style={styles.ApplicabilityContent} source={require('../assets/icon/Applicability2.png')}/>{itext}
             </Text>
         );
     };
@@ -40,87 +75,159 @@ const SingletonScreen = ({navigation, route}) => {
             </View>
         );
     };
-    const Singleton_En_Screen = Singleton_En;
+    const Singleton_Vn_Screen = Singleton_Vn;
     return (
-        <ScrollView>
-            {header_text('Overview')}
-            {image_content(Singleton_En_Screen.Image_1)}
+        <ScrollView showsVerticalScrollIndicator={false} style={styles.mainContainer}>
+            {header_text('Singleton')}
+            <Text>
+                <Image style={styles.header_icon} source={require('../assets/icon/Intent.png')}/>
+                {header1_text('Intent')}
+            </Text>
+            {content_text(Singleton_Vn_Screen.Intent[0])}
+            {image_content(Singleton_Vn_Screen.Image[0])}
 
-            {header1_text('Tên mẫu:')}
-            {content_text(Singleton_En_Screen.Overview_1)}
 
-            {header1_text('Phân loại:')}
-            {content_text(Singleton_En_Screen.Overview_2)}
-            {header1_text('Mục đích:')}
-            {content_text(Singleton_En_Screen.Overview_3)}
-            {content_text(Singleton_En_Screen.Overview_4)}
-            {content_text(Singleton_En_Screen.Overview_5)}
+            <Text>
+                <Image style={styles.header_icon} source={require('../assets/icon/Problem.png')}/>
+                {header1_text('Problem')}
+            </Text>
+            {content_text(Singleton_Vn_Screen.Problem[0])}
+            {num_content_text(Singleton_Vn_Screen.Problem[1])}
+            {num_content_text(Singleton_Vn_Screen.Problem[2])}
+            {num_content_text(Singleton_Vn_Screen.Problem[3])}
+            {image_content(Singleton_Vn_Screen.Image[1])}
 
-            {header_text('Motivation')}
-            {content_text(Singleton_En_Screen.Motivation_1)}
-            {content_text(Singleton_En_Screen.Motivation_2)}
-            {content_text('Ví dụ: ' + Singleton_En_Screen.Motivation_3)}
-            {image_content(Singleton_En_Screen.Image_2)}
-            {image_content(Singleton_En_Screen.Image_3)}
-            {image_content(Singleton_En_Screen.Image_4)}
+            {num_content_text(Singleton_Vn_Screen.Problem[4])}
+            {num_content_text(Singleton_Vn_Screen.Problem[5])}
+            {num_content_text(Singleton_Vn_Screen.Problem[6])}
+            {content_text(Singleton_Vn_Screen.Problem[7])}
 
-            {header_text('Characteristics')}
-            {image_content(Singleton_En_Screen.Image_5)}
-            {header1_text('Ý nghĩa từng thành viên:')}
-            {content_text(Singleton_En_Screen.Characteristics_1)}
-            {content_text(Singleton_En_Screen.Characteristics_2)}
-            {content_text(Singleton_En_Screen.Characteristics_3)}
+            <Text>
+                <Image style={styles.header_icon} source={require('../assets/icon/Solution.png')}/>
+                {header1_text('Solution')}
+            </Text>
+            {content_text(Singleton_Vn_Screen.Solution[0])}
+            {dot_content_text(Singleton_Vn_Screen.Solution[1])}
+            {dot_content_text(Singleton_Vn_Screen.Solution[2])}
+            {content_text(Singleton_Vn_Screen.Solution[3])}
 
-            {header_text('Consequences')}
-            {header1_text('Ý nghĩa từng thành viên:')}
-            {header1_text('Ưu điểm:')}
-            {content_text(Singleton_En_Screen.Consequences_1)}
-            {content_text(Singleton_En_Screen.Consequences_2)}
-            {content_text(Singleton_En_Screen.Consequences_3)}
-            {header1_text('Nhược điểm:')}
-            {content_text(Singleton_En_Screen.Consequences_4)}
-            {content_text(Singleton_En_Screen.Consequences_5)}
+            <Text>
+                <Image style={styles.header_icon} source={require('../assets/icon/RealWorld.png')}/>
+                {header1_text('Real-World Analogy')}
+            </Text>
+            {content_text(Singleton_Vn_Screen.RealWorldAnalogy[0])}
 
-            {header_text('Reality/Installation')}
-            {highlight_content_text(Singleton_En_Screen.Reality_1)}
-            {content_text(Singleton_En_Screen.Reality_2)}
-            {highlight_content_text(Singleton_En_Screen.Reality_3)}
-            {content_text(Singleton_En_Screen.Reality_4)}
-            {highlight_content_text(Singleton_En_Screen.Reality_5)}
-            {content_text(Singleton_En_Screen.Reality_6)}
-            {highlight_content_text(Singleton_En_Screen.Reality_7)}
-            {content_text(Singleton_En_Screen.Reality_8)}
-            {highlight_content_text(Singleton_En_Screen.Reality_9)}
-            {content_text(Singleton_En_Screen.Reality_10)}
-            {highlight_content_text(Singleton_En_Screen.Reality_11)}
-            {content_text(Singleton_En_Screen.Reality_12)}
+            <Text>
+                <Image style={styles.header_icon} source={require('../assets/icon/Structure.png')}/>
+                {header1_text('Structure')}
+            </Text>
+            {image_content(Singleton_Vn_Screen.Image[4])}
+            {num_content_text(Singleton_Vn_Screen.Structure[0])}
+            {num_content_text(Singleton_Vn_Screen.Structure[1])}
 
-            {header_text('Relations with Other Patterns')}
+            <Text>
+                <Image style={styles.header_icon} source={require('../assets/icon/Applicability.png')}/>
+                {header1_text('Applicability')}
+            </Text>
+            {bug_content_text(Singleton_Vn_Screen.Applicability[0])}
+            {lightN_content_text(Singleton_Vn_Screen.Applicability[1])}
+            {bug_content_text(Singleton_Vn_Screen.Applicability[2])}
+            {lightN_content_text(Singleton_Vn_Screen.Applicability[3])}
+            {content_text(Singleton_Vn_Screen.Applicability[4])}
+
+            <Text>
+                <Image style={styles.header_icon} source={require('../assets/icon/Implement.png')}/>
+                {header1_text('How to Implement')}
+            </Text>
+            {num_content_text(Singleton_Vn_Screen.HowtoImplement[0])}
+            {num_content_text(Singleton_Vn_Screen.HowtoImplement[1])}
+            {num_content_text(Singleton_Vn_Screen.HowtoImplement[2])}
+            {num_content_text(Singleton_Vn_Screen.HowtoImplement[3])}
+            {num_content_text(Singleton_Vn_Screen.HowtoImplement[4])}
+            {num_content_text(Singleton_Vn_Screen.HowtoImplement[5])}
+
+            <Text>
+                <Image style={styles.header_icon} source={require('../assets/icon/ProsCons.png')}/>
+                {header1_text('Pros and Cons')}
+            </Text>
+            {greenTick_content_text(Singleton_Vn_Screen.Pros[0])}
+            {greenTick_content_text(Singleton_Vn_Screen.Pros[1])}
+            {greenTick_content_text(Singleton_Vn_Screen.Pros[2])}
+            {redX_content_text(Singleton_Vn_Screen.Cons[0])}
+            {redX_content_text(Singleton_Vn_Screen.Cons[1])}
+            {redX_content_text(Singleton_Vn_Screen.Cons[2])}
+            {redX_content_text(Singleton_Vn_Screen.Cons[3])}
+
+            <Text>
+                <Image style={styles.header_icon} source={require('../assets/icon/Relations.png')}/>
+                {header1_text('Relations with Other Patterns:')}
+            </Text>
+            {dot_content_text(Singleton_Vn_Screen.RelationswithOtherPatterns[0])}
+            {dot_content_text(Singleton_Vn_Screen.RelationswithOtherPatterns[1])}
+            {content_text(Singleton_Vn_Screen.RelationswithOtherPatterns[2])}
+            {content_text(Singleton_Vn_Screen.RelationswithOtherPatterns[3])}
+            {dot_content_text(Singleton_Vn_Screen.RelationswithOtherPatterns[4])}
+
         </ScrollView>
-    )
-}
+    );
+};
 
 const styles = StyleSheet.create({
     header: {
         alignSelf: 'center',
         fontWeight: 'bold',
         color: 'black',
+        fontSize: 30,
     },
     header1: {
         fontWeight: 'bold',
         color: 'black',
+        fontSize: 30,
     },
     content: {
         color: 'black',
+        fontSize: 15,
+        marginTop: 15,
+    },
+    contentNum: {
+        color: 'black',
+        fontSize: 15,
+        marginLeft: 20,
+        marginTop: 10,
+    },
+    contentDot: {
+        color: 'black',
+        fontSize: 15,
+        marginLeft: 20,
+        marginTop: 10,
     },
     content_highlight: {
         color: 'blue',
+        fontSize: 15,
+        marginTop: 10,
     },
     image_content: {
         alignSelf: 'center',
         resizeMode: 'contain',
         width: '90%',
+        fontSize: 15,
     },
-})
+    mainContainer: {
+        marginLeft: 15,
+        marginRight: 15,
+    },
+    pros_cons_icon:{
+        width: 15,
+        height: 15,
+    },
+    header_icon:{
+        width: 35,
+        height: 35,
+    },
+    ApplicabilityContent:{
+        width: 30,
+        height: 30,
+    },
+});
 
-export default SingletonScreen
+export default SingletonScreen;
