c128b69c1fb36946bcc43ec0c2513f76bc384354
Trần Quang Huy <huy.com2003@gmail.com>
2024-06-22 13:25:17 +0700

    Add test Navigate

--- DesignAPP/screens/Catalog.js
+++ DesignAPP/screens/Catalog.js
@@ -1,5 +1,5 @@
 /* eslint-disable prettier/prettier */
-import { View, Text, ScrollView, Image, TouchableOpacity, StyleSheet, FlatList,TouchableWithoutFeedback, Animated } from 'react-native';
+import { View, Text, ScrollView, Image, TouchableOpacity, StyleSheet, FlatList,TouchableWithoutFeedback, Animated, Button } from 'react-native';
 import React, { useEffect, useState } from 'react';
 
 
@@ -9,6 +9,7 @@ const Catalog = ({navigation, route}) => {
     return(
         <View>
             <Text>Hello world - Catalog</Text>
+            <Button title="Navigate test to singelton" onPress={() => navigation.navigate('Singleton')}/>
         </View>
     )
 };
